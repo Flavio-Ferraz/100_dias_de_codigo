9cbee5524c24e923dc915e3a8f8b6140bad756a0
Flavio Ferraz <flavioferraz80@gmail.com>
2024-09-05 18:26:42 -0300

    Cógigo em Python que solicita e soma 2 números.

---
+++ Dia 2/main.py
@@ -0,0 +1,8 @@
+num1=float(input("Digite um número: "))
+num2=float(input("Digite outro número: "))
+
+def soma(num1, num2):
+    return num1+num2
+
+resultado = soma(num1, num2)
+print(f"A soma entre {num1} e {num2} é {resultado}")
