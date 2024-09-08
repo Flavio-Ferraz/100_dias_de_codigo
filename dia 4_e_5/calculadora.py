b524b16129db42e9d0057839b3633100726909fb
Flavio Ferraz <flavioferraz80@gmail.com>
2024-09-08 15:00:58 -0300

    Calculadora básica completa para o desafio 100 dias de código

--- dia 4_e_5/calculadora.py
+++ dia 4_e_5/calculadora.py
@@ -10,3 +10,23 @@ def calculadora():
     print("3 - Multiplicação (*)")
     print("4 - Divisão (/)")
     operacao = input("Digite o número da operação (1/2/3/4): ")
+    
+    if operacao == '1':
+        resultado = num1 + num2
+        print(f"O resultado de {num1} + {num2} é {resultado}")
+    elif operacao == '2':
+        resultado = num1 - num2
+        print(f"O resultado de {num1} - {num2} é {resultado}")
+    elif operacao == '3':
+        resultado = num1 * num2
+        print(f"O resultado de {num1} * {num2} é {resultado}")
+    elif operacao == '4':
+        if num2 != 0: #precisamos evitar a inclusão do 0 como divisor
+            resultado = num1 / num2
+            print(f"O resultado de {num1} / {num2} é {resultado}")
+        else:
+            print("Erro! Não é possível dividir por zero.")
+    else:
+        print("Operação inválida! Escolha uma opção entre 1 e 4.")
+    
+calculadora()
