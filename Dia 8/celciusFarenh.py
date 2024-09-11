f999e287728dc9c02738e36afe8444a1bb47b3dd
Flavio Ferraz <flavioferraz80@gmail.com>
2024-09-11 19:29:45 -0300

    Conversor de °Celcius para Farenheit

---
+++ Dia 8/celciusFarenh.py
@@ -0,0 +1,9 @@
+
+
+def convert ():
+    temp = float(input("Digite a temperatura em °Ceulcius: "))
+    Tf = ((temp/5)*9)+32
+    
+    print (f"A temperatura de {temp}°Celsius, representa {Tf}°Farenheit")
+
+convert()
